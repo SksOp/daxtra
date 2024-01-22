88711f9a0ea843cfed09ed563814dec230bee9ad
sks <shubhaman47@gmail.com>
2024-01-22 11:40:56 +0530

    updated name

--- components/introduction.tsx
+++ components/introduction.tsx
@@ -5,7 +5,7 @@ function Introduction() {
     <div className="flex  items-center justify-center w-full h-full">
       <div className=" max-w-md gap-3 border-2 rounded-md p-10 border-foreground-muted w-full">
         <h1 className="text-3xl font-bold text-center w-full">
-          Welcome to the Dextra Search!
+          Welcome to the <br /> Daxtra Search!
         </h1>
       </div>
     </div>
